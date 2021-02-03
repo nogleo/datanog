9679fce2748dbccab8c5280772997bf7d8400b65
nogleo <62610230+nogleo@users.noreply.github.com>
2021-02-03 20:57:26 -0300

    fix tabs

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from PyQt5 import uic
 from PyQt5.QtCore import pyqtSlot
 import datanog 
 
-dn = datanog.daq()
 
 class MplCanvas(FigureCanvas):
 	def __init__(self, parent=None, width=5, height=4, dpi=100):
@@ -21,12 +20,11 @@ class MplCanvas(FigureCanvas):
 		fig.tight_layout()
 
 class datanogAPP(QtWidgets.QMainWindow):
-	def __init__(self):
-		QtWidgets.QMainWindow.__init__(self)
-		self.ui = uic.loadUi('main.ui',self)
-		self.resize(800, 480)
-        
-		self.q = queue.Queue(maxsize=10000)
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        self.ui = uic.loadUi('main.ui',self)
+        self.resize(800, 480)
+        self.q = queue.Queue(maxsize=10000)
         dn = datanog.daq()
         self.pushButton.clicked.connect(self.pulldata)
 
