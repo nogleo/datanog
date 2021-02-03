9f05af212d1a791650687bf43174f47740fd6959
nogleo <62610230+nogleo@users.noreply.github.com>
2021-02-03 17:28:43 -0300

    base

---
+++ main.py
@@ -0,0 +1,33 @@
+import sys
+import matplotlib
+matplotlib.use('Qt5Agg')
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+import matplotlib.ticker as ticker
+import queue
+import numpy as np
+from PyQt5 import QtCore, QtWidgets,QtGui
+from PyQt5 import uic
+from PyQt5.QtCore import pyqtSlot
+import datanog 
+
+
+
+class MplCanvas(FigureCanvas):
+	def __init__(self, parent=None, width=5, height=4, dpi=100):
+		fig = Figure(figsize=(width, height), dpi=dpi)
+		self.axes = fig.add_subplot(111)
+		super(MplCanvas, self).__init__(fig)
+		fig.tight_layout()
+
+class datanogAPP(QtWidgets.QMainWindow):
+	def __init__(self):
+		QtWidgets.QMainWindow.__init__(self)
+		self.ui = uic.loadUi('main.ui',self)
+		self.resize(800, 480)
+
+
+app = QtWidgets.QApplication(sys.argv)
+mainWindow = datanogAPP()
+mainWindow.show()
+sys.exit(app.exec_())
