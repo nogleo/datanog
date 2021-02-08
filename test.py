e20ca21507e2f1e9fb2e18282df071fbf9d85de2
nogleo <62610230+nogleo@users.noreply.github.com>
2021-02-08 10:18:41 -0300

    fixing save test data

--- test.py
+++ test.py
@@ -2,19 +2,30 @@ import queue
 import asyncio
 import datanog
 import time
+import numpy as np
+import os
 
 dn = datanog.daq()
 
-q = queue.Queue(maxsize=10000)
+q = queue.Queue()
 
-def pulldata():
+def pulldata(_size = 1):
         i=0
         t0=tf = time.perf_counter()
-        while i<10000:
+        while i< _size//dn.dt:
             ti=time.perf_counter()
             if ti-tf>=dn.dt:
                 tf = ti
                 i+=1
-                q.put(dn.pull(dn.devices[0]))
+                self.q.put(dn.pull(dn.devices[0]))
         t1 = time.perf_counter()
         print(t1-t0)
+
+def savedata():
+    if 'DATA' not in os.listdir():
+        os.mkdir('DATA')
+    data = []
+    while q.qsize()>0:
+        data.append(q.get())
+    arr = np.array(data)
+    np.save('test{}.npy'.format(len(os.listdir('DATA'))), arr)
