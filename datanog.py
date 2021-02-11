7b77c50f12248f9bf98e51e9c29d27067930cb06
nogleo <62610230+nogleo@users.noreply.github.com>
2021-02-11 12:56:13 -0300

    reduce overhead

--- datanog.py
+++ datanog.py
@@ -52,10 +52,10 @@ class daq:
                     print("ERROR: ",e)
 
     def pull(self):
-        _p = []
-        for _d in self.devices:
-            _p=+unpack(_d[3],bytearray( self.bus.read_i2c_block_data(_d[0],_d[1], _d[2])))
-        return _p
+        return unpack(self.devices[0][3],bytearray( self.bus.read_i2c_blockself.devices[0]ata(self.devices[0][0],self.devices[0][1], self.devices[0][2]))) + /\
+            unpack(self.devices[1][3],bytearray( self.bus.read_i2c_blockself.devices[1]ata(self.devices[1][0],self.devices[1][1], self.devices[1][2])))
+
+        
 
     def pulldata(self, _size = 3):
         self.q = queue.Queue()
