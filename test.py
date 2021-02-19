1684aa64e444cd454768ba0e8cd8ace76ebe380e
nogleo <leo.nog88@gmail.com>
2021-02-19 13:38:12 -0300

    pull

--- test.py
+++ test.py
@@ -7,33 +7,7 @@ import os
 
 dn = datanog.daq()
 
-q = queue.Queue()
 
-dn.pulldata()
+dn.pulldata(1)
 
-
-def pulldata(_size = 1):
-        i=0
-        t0=tf = time.perf_counter()
-        while i< _size//dn.dt:
-            ti=time.perf_counter()
-            if ti-tf>=dn.dt:
-                tf = ti
-                i+=1
-                q.put(dn.pull(dn.devices[0]))
-        t1 = time.perf_counter()
-        print(t1-t0)
-
-def savedata():
-    if 'DATA' not in os.listdir():
-        os.mkdir('DATA')
-    data = []
-    while q.qsize()>0:
-        data.append(q.get())
-    arr = np.array(data)
-    os.chdir('DATA')
-    np.save('data{}.npy'.format(len(os.listdir())), arr)
-    os.chdir('..')
-
-pulldata(2)
-savedata()
+dn.pulldata2(1)
