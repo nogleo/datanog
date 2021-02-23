cbb487f16ea7106d84ab0b41877663ec0fe4d7a8
nogleo <62610230+nogleo@users.noreply.github.com>
2021-02-23 12:08:05 -0300

    i print again

--- datanog.py
+++ datanog.py
@@ -240,7 +240,7 @@ class daq:
                     t1 = time.perf_counter()
                 print(t1-t0)
             except Exception as e:
-                print(e)    
+                print(i)    
 
         
 
