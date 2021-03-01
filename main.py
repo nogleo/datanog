4c1ef2df150193561dd117e34dd6629c008d6309
nogleo <62610230+nogleo@users.noreply.github.com>
2021-03-01 17:04:54 -0300

    alpha test

--- main.py
+++ main.py
@@ -1,6 +1,32 @@
 import datanog as nog
-import gui
+from gui import Ui_MainWindow
+
+from PyQt5 import QtCore as qtc
+from PyQt5 import QtWidgets as qtw
 
 dn = nog.daq()
+class appnog(qtw.QMainWindow):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.startbutton.clicked.connect(self.collect)
+        self.ui.stopbutton.clicked.connect(self.interrupt)
+
+    def collect(self):
+        dn.savedata(dn.pulldata(self.ui.label.text()))
 
 
+    def interrupt(self):
+        dn.state = False
+
+
+
+
+if __name__ == '__main__':
+    app = qtw.QApplication([])
+    widget = appnog()
+    widget.show()
+    app.exec_()
