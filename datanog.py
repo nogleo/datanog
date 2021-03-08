dfcbd44df87a793cd7b2e8d2e04c92e021eb4f3a
nogleo <62610230+nogleo@users.noreply.github.com>
2021-03-08 19:35:49 -0300

    fix adc config, lets hope

--- datanog.py
+++ datanog.py
@@ -59,7 +59,8 @@ class daq:
                 except Exception as e:
                     print("ERROR: ",e)
         elif _device == 0x48:
-            _settings = [0x01, [(5<<11 | 0<<8)>>8 & 0xFF, (5<<11 | 0<<8) & 0xFF]]
+            _config = (5<<9 | 0<<8 | 4<<5 | 3)
+            _settings = [0x01, [_config>>8 & 0xFF, _config & 0xFF]]
             self.bus.write_byte_data(_device, _settings[0], _settings[1])
 
         
