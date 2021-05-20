f9991377267c16d2a099b99222cefc1ee8efc341
nogleo <62610230+nogleo@users.noreply.github.com>
2021-05-20 11:09:21 -0300

    go go

--- datanog.py
+++ datanog.py
@@ -12,7 +12,7 @@ root = os.getcwd()
 
 
 class daq:
-    def __init__(self):
+    def __init__(self, fs=1660):
         self.__name__ = "daq"
         try:
             self.bus = SMBus(1)
@@ -21,14 +21,14 @@ class daq:
             print("ERROR ", e)
 
         self.dev = []
-        self.fs = 1660
+        self.fs = fs
         self.dt = 1/self.fs
         self.state = 1
         self.raw = 1
         self.G = 9.81
         self.Rot = np.pi
         
-        self.odr = 10  #8=1660Hz 9=3330Hz 10=6660Hz
+        self.odr = 9  #8=1660Hz 9=3330Hz 10=6660Hz
         self.range = [1, 3]     #[16G, 2000DPS]
         for device in range(128):
             try:
